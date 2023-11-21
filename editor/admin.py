d07352b0a0df9fe62b7b89861125e2033312f143
Victor-PC <filpsixi@mail.ru>
2023-11-21 06:49:50 +0300

    hotfix

--- editor/admin.py
+++ editor/admin.py
@@ -1,36 +1,65 @@
 import itertools
 from django.contrib import admin
+from django.db.models import F
 from django import forms
 from main.models import MenuItem, Menu
-
-
-def get_initial():
-    pass
 
 
 class CustomMenuItemForm(forms.ModelForm):
     class Meta:
         model = MenuItem
         exclude = ("child_item",)
-
-    parent_item = forms.ChoiceField(
-        choices=itertools.chain([("", "----",)], map(lambda x: (x.pk, x,), MenuItem.objects.all())),
-        required=False)
+    parent_item = forms.ChoiceField(required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        parent = MenuItem.objects.filter(child_item_id=self.instance.id)
-        self.initial["parent_item"] = (parent[0].pk, parent[0],) if parent.count() else ("", "----",)
+        parent = MenuItem.objects.filter(child_item=self.instance)
+        if parent.count():
+            self.fields["parent_item"].choices = itertools.chain([("", "----",)],
+                                                                 map(lambda x: (x.pk, x,),
+                                                                     MenuItem.objects.filter(menu_id=self.instance.menu_id)))
+            self.initial["parent_item"] = (parent[0].pk, parent[0],)
+        else:
+            self.fields["parent_item"].choices = itertools.chain([("", "----",)],
+                                                                 map(lambda x: (x.pk, x,),
+                                                                     MenuItem.objects.filter(menu__isnull=True)))
+            self.initial["parent_item"] = ("", "----",)
 
     def clean(self):
         cleaned_data = super().clean()
-        parent_item = cleaned_data.get("parent_item", None)
-        if parent_item:
-            del cleaned_data["parent_item"]
-        if not parent_item:
+        parent_item_pk = cleaned_data.get("parent_item", None)
+        if not parent_item_pk:
             return super().clean()
-        MenuItem.objects.filter(id=parent_item).update(child_item=self.instance.pk)
+        self._check_circular_parent_child_relationship(self.instance, parent_item_pk)
+        self._check_self_child_relationship(self.instance, parent_item_pk)
+        MenuItem.objects.filter(id=parent_item_pk).update(child_item=self.instance.pk)
         return super().clean()
+
+    def clean_column_index(self):
+        index_by_current_level = self.cleaned_data["column_index"]
+        if not index_by_current_level:  # default
+            last_item = MenuItem.objects.filter(menu_id=self.instance.menu_id).order_by("column_index")
+            return last_item.last().column_index if last_item.count() else 0
+        if not MenuItem.objects.filter(column_index=index_by_current_level - 1).count():
+            raise forms.ValidationError("Указанный индекс слишком велик")
+        items_with_highest_co_index = MenuItem.objects.filter(column_index__gte=self.instance.column_index)\
+            .order_by("column_index")
+        val = items_with_highest_co_index.last().column_index
+        if items_with_highest_co_index.count():  # Сместить в право на 1 элементы старше редактируемого
+            items_with_highest_co_index.update(column_index=F("column_index") + 1)
+        return val
+
+    @staticmethod
+    def _check_circular_parent_child_relationship(item, parent_item: str):
+        """ Валидация на предмет замыкания: item.child_item = other_item AND other_item.child_item == item """
+        if item.pk == parent_item and MenuItem.objects.filter(id=parent_item).child_item == item.pk:
+            raise forms.ValidationError("Циклическое замыкание между двумя записами", code="invalid")
+
+    @staticmethod
+    def _check_self_child_relationship(item, parent_item: str):
+        """ Валидация на предмет замыкания ноды самой на себя: item == item.child_item """
+        if item.pk == parent_item:
+            raise forms.ValidationError("Замыкание ноды саму на себя", code="invalid")
 
 
 class ItemsModelAdmin(admin.ModelAdmin):
--- main/models.py
+++ main/models.py
@@ -2,17 +2,19 @@ from django.db import models
 
 
 class Menu(models.Model):
-    name = models.CharField(max_length=10, blank=False, unique=True, db_index=True)
+    name = models.CharField(max_length=10, blank=False, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class MenuItem(models.Model):
     inner_text = models.CharField(max_length=100, blank=False, unique=True)
-    level_index = models.SmallIntegerField(default=0, db_index=True)
     column_index = models.SmallIntegerField(default=0, db_index=True)
-    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, to_field="name")
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, to_field="name", blank=True, null=True)
     child_item = models.ForeignKey("self", on_delete=models.CASCADE, null=True, default=None, blank=True)
     hidden = models.BooleanField(blank=False, default=False)
-    href = models.URLField()
+    href = models.URLField(unique=True)
 
     def __str__(self):
         return f"{self.inner_text} ({self.href})"
