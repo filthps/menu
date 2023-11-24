82a0e202e047b2cbcb54b633300d0eb2df13a70d
Victor-PC <filpsixi@mail.ru>
2023-11-24 15:35:19 +0300

    models.py

--- main/models.py
+++ main/models.py
@@ -9,12 +9,12 @@ class Menu(models.Model):
 
 
 class MenuItem(models.Model):
-    inner_text = models.CharField(max_length=100, blank=False, unique=True)
+    label = models.CharField(max_length=100, blank=False, unique=True)
     column_index = models.SmallIntegerField(default=0, db_index=True)
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE, to_field="name", blank=True, null=True)
     child_item = models.ForeignKey("self", on_delete=models.CASCADE, null=True, default=None, blank=True)
-    hidden = models.BooleanField(blank=False, default=False)
+    hidden = models.BooleanField(blank=False, default=True)
     href = models.URLField(unique=True)
 
     def __str__(self):
-        return f"{self.inner_text} ({self.href})"
+        return f"{self.label} ({self.href})"
